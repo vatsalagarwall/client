33e4e4dcf6fe48e387d31817e7bb260b7b616b61
Vatsal Agarwal <bindalvatsaljpr@gmail.com>
2023-12-19 16:41:40 +0530

    commit

--- src/Components/Contact.jsx
+++ src/Components/Contact.jsx
@@ -13,7 +13,7 @@ function Contact() {
       >
         <div className="col-md-6">
           <h2>Contact Me</h2>
-          <p style={{ color: "white" }}>
+          <p style={{ color: "white", marginTop: "40px" }}>
             <svg
               xmlns="http://www.w3.org/2000/svg"
               width="24"
@@ -36,7 +36,29 @@ function Contact() {
               bindalvatsaljpr@gmail.com
             </a>
           </p>
-          <p>
+          <p
+            href="tel:+1234567890"
+            style={{ color: "white", marginTop: "40px" }}
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="currentColor"
+              stroke-width="2"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              className="feather feather-phone-call"
+            >
+              <path d="M22 16.92v3a2 2 0 0 1-2.18 2 19.79 19.79 0 0 1-8.63-3.07 19.5 19.5 0 0 1-6-6 19.79 19.79 0 0 1-3.07-8.67A2 2 0 0 1 4.11 2h3a2 2 0 0 1 2 1.72 12.84 12.84 0 0 0 .7 2.81 2 2 0 0 1-.45 2.11L8.09 9.91a16 16 0 0 0 6 6l1.27-1.27a2 2 0 0 1 2.11-.45 12.84 12.84 0 0 0 2.81.7A2 2 0 0 1 22 16.92z" />
+              <path d="M14.05 2a9 9 0 0 1 8 7.94" />
+              <path d="M14.05 6A5 5 0 0 1 18 10" />
+            </svg>
+            +91 8888888888
+          </p>
+          <div style={{ marginTop: "40px" }}>
             <a
               href="https://www.linkedin.com/in/vatsal-agarwall"
               target="_blank"
@@ -59,12 +81,11 @@ function Contact() {
                 <circle cx="4" cy="4" r="2" />
               </svg>
             </a>
-          </p>
-          <p>
+
             <a
-              href="https://github.com/your-github-username"
+              href="https://github.com/vatsalagarwall"
               target="_blank"
-              style={{ color: "white" }}
+              style={{ color: "white", marginLeft: "30px" }}
             >
               <svg
                 xmlns="http://www.w3.org/2000/svg"
@@ -82,28 +103,13 @@ function Contact() {
                 <path d="M9 18c-4.51 2-5-2-7-2" />
               </svg>
             </a>
-          </p>
-          <p>
-            <a href="tel:+1234567890" style={{ color: "white" }}>
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="24"
-                height="24"
-                viewBox="0 0 24 24"
-                fill="none"
-                stroke="currentColor"
-                stroke-width="2"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                className="feather feather-phone-call"
-              >
-                <path d="M22 16.92v3a2 2 0 0 1-2.18 2 19.79 19.79 0 0 1-8.63-3.07 19.5 19.5 0 0 1-6-6 19.79 19.79 0 0 1-3.07-8.67A2 2 0 0 1 4.11 2h3a2 2 0 0 1 2 1.72 12.84 12.84 0 0 0 .7 2.81 2 2 0 0 1-.45 2.11L8.09 9.91a16 16 0 0 0 6 6l1.27-1.27a2 2 0 0 1 2.11-.45 12.84 12.84 0 0 0 2.81.7A2 2 0 0 1 22 16.92z" />
-                <path d="M14.05 2a9 9 0 0 1 8 7.94" />
-                <path d="M14.05 6A5 5 0 0 1 18 10" />
-              </svg>
-            </a>
-          </p>
-          <button style={{ backgroundColor: "black", color: "white" }}>
+          </div>
+          <br />
+          <button
+            type="submit"
+            class="btn"
+            style={{ borderColor: "red", color: "white", marginTop: "20px" }}
+          >
             Download CV
           </button>
         </div>
@@ -147,7 +153,11 @@ function Contact() {
                   rows="3"
                 ></textarea>
               </div>
-              <button type="submit" class="btn">
+              <button
+                type="submit"
+                class="btn"
+                style={{ borderColor: "red", color: "white" }}
+              >
                 Submit
               </button>
             </form>
