84e21d675ccfee2b323408718d2da0a94b4025f2
Vatsal Agarwal <bindalvatsaljpr@gmail.com>
2023-12-19 16:08:15 +0530

    commit

--- src/Components/Contact.jsx
+++ src/Components/Contact.jsx
@@ -2,141 +2,155 @@ import React from "react";
 import { useState } from "react";
 import { Link } from "react-router-dom";
 function Contact() {
-  const [activeTab, setActiveTab] = useState("Education");
-
-  const renderContent = () => {
-    switch (activeTab) {
-      case "Skills":
-        return (
-          <div className="text-white" style={{ marginTop: "10px" }}>
-            <ul>
-              <li>
-                <span style={{ color: "red" }}>Technical Skills: </span> • C •
-                C++ • Python • JavaScript • MERN stack
-              </li>
-              <li>
-                <span style={{ color: "red" }}>Other Skills:</span> • Firebase •
-                MySQL
-              </li>
-              <li>
-                <span style={{ color: "red" }}>Coursework:</span> • Database
-                Management Systems • Data Structures and Algorithms •
-                Object-Oriented Programming • Operating Systems • Computer
-                Network
-              </li>
-            </ul>
-          </div>
-        );
-      case "Experience":
-        return (
-          <div className="text-white" style={{ marginTop: "10px" }}>
-            <ul>
-              <li>
-                Web Development Intern at{" "}
-                <Link
-                  to="https://eplanetsoft.com/"
-                  target="_blank"
-                  style={{ textDecoration: "none", color: "white" }}
-                >
-                  EPlanet Soft – EP SOFT Pvt. Ltd.
-                </Link>
-                <br />
-                <span style={{ fontSize: "smaller", color: "red" }}>
-                  Sep 2023 – Dec 2023
-                </span>
-              </li>
-              <li>
-                Member of Technical Team in Apple Developers Group
-                <br />
-                <span style={{ fontSize: "smaller", color: "red" }}>
-                  Dec 2021 – present
-                </span>
-              </li>
-            </ul>
-          </div>
-        );
-
-      case "Education":
-        return (
-          <div className="text-white" style={{ marginTop: "10px" }}>
-            <ul>
-              <li>
-                Vellore Institute of Technology, Vellore{" "}
-                <span style={{ fontSize: "smaller" }}>
-                  (B.Tech in Information Technology)
-                </span>
-                <br />
-                <span
-                  style={{
-                    fontSize: "smaller",
-                    color: "red",
-                  }}
-                >
-                  Expected July 2025
-                </span>
-              </li>
-              <li>
-                Ryan International School, Jaipur{" "}
-                <span style={{ fontSize: "smaller" }}>
-                  (Secondary Education)
-                </span>
-                <br />
-                <span
-                  style={{
-                    fontSize: "smaller",
-                    color: "red",
-                  }}
-                >
-                  2019-2020
-                </span>
-              </li>
-            </ul>
-          </div>
-        );
-      default:
-        return null;
-    }
-  };
-
   return (
     <div
       className="container-fluid text-white p-4"
       style={{ backgroundColor: "black" }}
     >
-      <div className="row" style={{ marginTop: "60px", marginBottom: "60px" }}>
+      <div
+        className="row"
+        style={{ marginTop: "80px", marginBottom: "80px", marginLeft: "40px" }}
+      >
         <div className="col-md-6">
           <h2>Contact Me</h2>
-          <p className="text-wrap">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla nec
-            purus feugiat, molestie ipsum et, consectetur justo. Integer
-            tristique odio id elit tempus, vel malesuada est bibendum. Lorem
-            ipsum dolor sit amet, consectetur adipiscing elit. Nulla nec purus
-            feugiat, molestie ipsum et, consectetur justo. Integer tristique
-            odio id elit tempus, vel malesuada est bibendum.
+          <p style={{ color: "white" }}>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="currentColor"
+              stroke-width="2"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              className="feather feather-mail"
+            >
+              <rect width="20" height="16" x="2" y="4" rx="2" />
+              <path d="m22 7-8.97 5.7a1.94 1.94 0 0 1-2.06 0L2 7" />
+            </svg>
+            <a
+              href="mailto:bindalvatsaljpr@gmail.com"
+              style={{ color: "white" }}
+            >
+              bindalvatsaljpr@gmail.com
+            </a>
           </p>
           <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla nec
-            purus feugiat, molestie ipsum et, consectetur justo. Integer
-            tristique odio id elit tempus, vel malesuada est bibendum. Lorem
+            <a
+              href="https://www.linkedin.com/in/vatsal-agarwall"
+              target="_blank"
+              style={{ color: "white" }}
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="24"
+                height="24"
+                viewBox="0 0 24 24"
+                fill="none"
+                stroke="currentColor"
+                stroke-width="2"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                className="feather feather-linkedin"
+              >
+                <path d="M16 8a6 6 0 0 1 6 6v7h-4v-7a2 2 0 0 0-2-2 2 2 0 0 0-2 2v7h-4v-7a6 6 0 0 1 6-6z" />
+                <rect width="4" height="12" x="2" y="9" />
+                <circle cx="4" cy="4" r="2" />
+              </svg>
+            </a>
           </p>
+          <p>
+            <a
+              href="https://github.com/your-github-username"
+              target="_blank"
+              style={{ color: "white" }}
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="24"
+                height="24"
+                viewBox="0 0 24 24"
+                fill="none"
+                stroke="currentColor"
+                stroke-width="2"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                className="feather feather-github"
+              >
+                <path d="M15 22v-4a4.8 4.8 0 0 0-1-3.5c3 0 6-2 6-5.5.08-1.25-.27-2.48-1-3.5.28-1.15.28-2.35 0-3.5 0 0-1 0-3 1.5-2.64-.5-5.36-.5-8 0C6 2 5 2 5 2c-.3 1.15-.3 2.35 0 3.5A5.403 5.403 0 0 0 4 9c0 3.5 3 5.5 6 5.5-.39.49-.68 1.05-.85 1.65-.17.6-.22 1.23-.15 1.85v4" />
+                <path d="M9 18c-4.51 2-5-2-7-2" />
+              </svg>
+            </a>
+          </p>
+          <p>
+            <a href="tel:+1234567890" style={{ color: "white" }}>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="24"
+                height="24"
+                viewBox="0 0 24 24"
+                fill="none"
+                stroke="currentColor"
+                stroke-width="2"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                className="feather feather-phone-call"
+              >
+                <path d="M22 16.92v3a2 2 0 0 1-2.18 2 19.79 19.79 0 0 1-8.63-3.07 19.5 19.5 0 0 1-6-6 19.79 19.79 0 0 1-3.07-8.67A2 2 0 0 1 4.11 2h3a2 2 0 0 1 2 1.72 12.84 12.84 0 0 0 .7 2.81 2 2 0 0 1-.45 2.11L8.09 9.91a16 16 0 0 0 6 6l1.27-1.27a2 2 0 0 1 2.11-.45 12.84 12.84 0 0 0 2.81.7A2 2 0 0 1 22 16.92z" />
+                <path d="M14.05 2a9 9 0 0 1 8 7.94" />
+                <path d="M14.05 6A5 5 0 0 1 18 10" />
+              </svg>
+            </a>
+          </p>
+          <button style={{ backgroundColor: "black", color: "white" }}>
+            Download CV
+          </button>
         </div>
 
         <div className="col-md-6">
           <div style={{ marginRight: "70px" }}>
-            <h2>Contact Me</h2>
-            <p className="text-wrap">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla nec
-              purus feugiat, molestie ipsum et, consectetur justo. Integer
-              tristique odio id elit tempus, vel malesuada est bibendum. Lorem
-              ipsum dolor sit amet, consectetur adipiscing elit. Nulla nec purus
-              feugiat, molestie ipsum et, consectetur justo. Integer tristique
-              odio id elit tempus, vel malesuada est bibendum.
-            </p>
-            <p>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla nec
-              purus feugiat, molestie ipsum et, consectetur justo. Integer
-              tristique odio id elit tempus, vel malesuada est bibendum. Lorem
-            </p>
+            {/* <h2>Contact Me</h2> */}
+            <form>
+              <div class="mb-3">
+                <label for="exampleInputName" class="form-label">
+                  Your Name
+                </label>
+                <input
+                  type="text"
+                  class="form-control"
+                  id="exampleInputName"
+                  aria-describedby="nameHelp"
+                />
+              </div>
+              <div class="mb-3">
+                <label for="exampleInputEmail" class="form-label">
+                  Email address
+                </label>
+                <input
+                  type="email"
+                  class="form-control"
+                  id="exampleInputEmail"
+                  aria-describedby="emailHelp"
+                />
+                <div id="emailHelp" class="form-text">
+                  We'll never share your email with anyone else.
+                </div>
+              </div>
+              <div class="mb-3">
+                <label for="exampleMessage" class="form-label">
+                  Your Message
+                </label>
+                <textarea
+                  class="form-control"
+                  id="exampleMessage"
+                  rows="3"
+                ></textarea>
+              </div>
+              <button type="submit" class="btn">
+                Submit
+              </button>
+            </form>
           </div>
         </div>
       </div>
